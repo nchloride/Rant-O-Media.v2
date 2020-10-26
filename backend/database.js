4150a3f15510f82fd982a039a647d7bd769159e6
nchloride <clankz39@gmail.com>
2020-10-26 15:15:58 +0800

    cluster database added

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const monk = require("monk");
 const mongo = require("mongodb").MongoClient;
 const objectId = require("mongodb").ObjectID;
-const db = monk(process.env.MONGO_URI ||'localhost:27017/testing');
+const db = monk("mongodb+srv://carlo_123:Wh2ldzBvCyFsokNM@cluster0.yigkf.mongodb.net/testing?retryWrites=true&w=majority" ||'localhost:27017/testing');
 
 module.exports = db;
