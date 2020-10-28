9dad7b2e25f2c7f8a28bc0b53caac3a091393d74
nchloride <clankz39@gmail.com>
2020-10-29 03:06:38 +0800

    removed MongoDB URI

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,7 @@
 const monk = require("monk");
+require('dotenv').config;
 const mongo = require("mongodb").MongoClient;
 const objectId = require("mongodb").ObjectID;
-const db = monk("mongodb+srv://carlo_123:Wh2ldzBvCyFsokNM@cluster0.yigkf.mongodb.net/testing?retryWrites=true&w=majority" ||'localhost:27017/testing');
+const db = monk(process.env.MONGO_URI ||'localhost:27017/testing');
 
 module.exports = db;
